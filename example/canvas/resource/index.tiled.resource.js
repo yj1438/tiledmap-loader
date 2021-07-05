a9d618e1fb876fce7517906ee305d9fed80e3110
沐瑜 <yj138290@antfin.com>
2021-07-05 20:39:19 +0800

    u

--- example/canvas/resource/index.tiled.resource.js
+++ example/canvas/resource/index.tiled.resource.js
@@ -1,14 +1,4 @@
 
-import image_images_baba_bg_png from './images/baba-bg.png';
-import image_images_bubble_png from './images/bubble.png';
-import image_images_ch_png from './images/ch.png';
-import image_images_icon_png from './images/icon.png';
-import image_images_panda_png from './images/panda.png';
 
 export default {
-  'images/baba-bg.png': image_images_baba_bg_png,
-  'images/bubble.png': image_images_bubble_png,
-  'images/ch.png': image_images_ch_png,
-  'images/icon.png': image_images_icon_png,
-  'images/panda.png': image_images_panda_png,
 }
